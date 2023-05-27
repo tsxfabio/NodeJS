1cd92b9e00289838614dd6f62fbcf43b1d60c19f
Fabio Leardini <leardinifabio90@gmail.com>
2023-05-27 00:35:08 -0300

    atualizando database e persistencia

--- src/server.js
+++ src/server.js
@@ -1,27 +1,32 @@
 import http from "node:http";
 import { json } from "./middleware/json.js";
+import { Database } from "./database.js";
 
-/* 
-Importação feita via CommonJS (usando require)
-const http = require("http");
-*/
-
-const tasks = [];
+const database = new Database();
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
+
   await json(req, res);
 
   if (method === "GET" && url === "/tasks") {
+    const tasks = database.select("tasks");
     return res.end(JSON.stringify(tasks));
   }
 
   if (method === "POST" && url === "/tasks") {
-    tasks.push({
+    const { title, description } = req.body;
+
+    const task = {
       id: 1,
-      title: "Primeira Task",
-      description: "Adicionando a primeira task na API",
-    });
+      title,
+      description,
+      created_at: new Date(),
+      completed_at: null,
+      updated_at: null,
+    };
+
+    database.insert("tasks", task);
 
     return res.end();
   }
